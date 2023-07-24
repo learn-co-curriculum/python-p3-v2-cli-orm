f9d0a6d071778d30e488f50b56d1b5e531b34784
Linda Seiter <linseiter@gmail.com>
2023-07-24 14:25:49 -0400

    add solution code

--- lib/helpers.py
+++ lib/helpers.py
@@ -10,27 +10,60 @@ def exit_program():
 
 
 def list_departments():
-    pass
+    departments = Department.get_all()
+    for department in departments:
+        print(department)
 
 
 def find_department_by_name():
-    pass
+    name = input("Enter the department's name: ")
+    department = Department.find_by_name(name)
+    print(department) if department else print(
+        f'Department {name} not found')
 
 
 def find_department_by_id():
-    pass
+    id = input("Enter the department's id: ")
+    department = Department.find_by_id(id)
+    print(department) if department else print(f'Department {id} not found')
 
 
 def create_department():
-    pass
+    name = input("Enter the department's name: ")
+    location = input("Enter the department's location: ")
+    try:
+        department = Department.create(name, location)
+        print(f'Success: {department}')
+    except Exception as exc:
+        print("Error creating department: ", exc)
 
 
 def update_department():
-    pass
+    id = input("Enter the department's id: ")
+    department = Department.find_by_id(id)
+    if department:
+        try:
+            name = input("Enter the department's new name: ")
+            department.name = name
+            location = input("Enter the department's new location: ")
+            department.location = location
+
+            department.update()
+            print(f'Success: {department}')
+        except Exception as exc:
+            print("Error updating department: ", exc)
+    else:
+        print(f'Department {id} not found')
 
 
 def delete_department():
-    pass
+    id = input("Enter the department's id: ")
+    department = Department.find_by_id(id)
+    if department:
+        department.delete()
+        print(f'Department {id} deleted')
+    else:
+        print(f'Department {id} not found')
 
 
 # You'll implement the employee functions in the lab
